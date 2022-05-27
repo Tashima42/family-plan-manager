63f52b8a7b992297522ebf89e5125076dfb3f0aa
Pedro Tashima <ptashima@toolboxtve.com>
2022-05-27 08:14:18 -0300

    fix(make-payment): remove unused parameter

--- src/use-cases/make-payment/index.ts
+++ src/use-cases/make-payment/index.ts
@@ -2,17 +2,15 @@ import {MakePaymentController} from "./make-payment-controller";
 import {MakePaymentUseCase} from "./make-payment-use-case";
 import {SqliteDatabase} from "../../repositories/implementations/Sqlite/index";
 import {SqliteGroupRepository} from "../../repositories/implementations/Sqlite/SqliteGroupPlanRepository";
-import {SqliteUserRepository} from "../../repositories/implementations/Sqlite/SqliteUserRepository";
 import {SqlitePaymentRepository} from "../../repositories/implementations/Sqlite/SqlitePaymentRepository";
 
 // Instantiate repositories
 const sqliteDatabase = new SqliteDatabase()
 const groupRepository = new SqliteGroupRepository(sqliteDatabase)
-const userRepository = new SqliteUserRepository(sqliteDatabase)
 const paymentRepository = new SqlitePaymentRepository(sqliteDatabase)
 
 // Instantiate Use Case
-const makePaymentUseCase = new MakePaymentUseCase(paymentRepository, groupRepository, userRepository)
+const makePaymentUseCase = new MakePaymentUseCase(paymentRepository, groupRepository)
 
 // Instantiate Controller
 const makePaymentController = new MakePaymentController(makePaymentUseCase)
